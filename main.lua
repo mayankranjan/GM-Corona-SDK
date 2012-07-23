3246e35296a9e343da9beee2eff01c3fecc5b47c
Ali Hamidi <ahamidi@me.com>
2012-07-23 16:15:40 +0400

    Updated app.lua to use events

--- main.lua
+++ main.lua
@@ -15,98 +15,27 @@ _H = display.contentHeight
 local gm = require("gameminion")
 local json = require("json")
 
+local friendID
+
 -- initialize Game Minion
--- Make sure you use your own games keys!
-gm = gm.init("bc1a9793a0a85ae9bca657dcb48faa90f73689fe", "d0731e1dabdfcf60336098ff75f3913230f2db89")
+-- Make sure you use your own Access and Secret games keys!
+gm = gm.init("XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX", "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX")
+
+gm:loginAPI("janedoe@mailmail.com","password")
 
 
--- create new user account or login existing
---gm:registerUser("Joe", "Blogs", "jbloggs", "jbloggs@mailmail.com", "password")
-gm:loginAPI("jbloggs@mailmail.com", "password")
 
--- analytics event table
-local analyticsEvent = {
-	event_type = "Game",
-	message = "New Game Started",
-	name = "Message Name",
-	value = ""
-}
-
--- network save json
-local saveData = {
-	["Level 1"] = 1,
-	["Level 2"] = 3,
-	["Level 3"] = 3,
-	["Sound"] = false
-}
-
-local saveDataBlob = json.encode(saveData)
-
--- Event listener looking for NetResponse Event
-local function netResListener(event)
-	print("Event Received: "..event.target)
+--As a security measure and standard practice we return an error, but we never reveal if the credencials are incorrect or the user doesn't exist
+local function listenerLogin(event)
+        if (event.errorMsg ~= nil) then         
+        	print( "Error Message: "..event.errorMsg)
+        end
+        --gm:updateMyProfile(nil,nil,"Doe2")
+        gm:getMyProfile()
 end
 
-Runtime:addEventListener("netResponse", netResListener)
 
-
-local function gmCallListener()
-	gm:getMyProfile()
-	--gm:getUserProfile("4ead491c5ceaa10001000015")
-	--gm:addFriend("4f0be09cc8c1950001000001")
-	--gm:getFriends()
-	--gm:removeFriend("4f0be09cc8c1950001000001")
-
-	--gm:getLeaderboards()
-	--gm:getLeaderboard("4f6f1e7e6b789d0001000008")
-	--gm:submitHighScore("4f6f1e736b789d0001000006", 105)
-	
-	--gm:getAchievements()
-	--gm:unlockAchievement("4f705899f903950001000016")
-	--gm:getMyAchievements()
-	--gm:getAchievement("4f705899f903950001000016")
-	--gm:submitEvent(analyticsEvent)
-	
-	--gm:getNews()
-	--gm:getUnreadNews()
-	--gm:getNewsArticle("4f70630897e9160001000006")
-	
-	--gm:registerDevice("11111111111111111")
-	--gm:createCloudBox()
-	--gm:networkSave(saveDataBlob, "4f8d29770c343d000100000c")
-	--gm:getCloudStorage()
-	
-	--gm:createChatRoom("test2")
-	--gm:getChatRooms()
-	--gm:getUsersInChatRoom("4f7172b3a821ab0001000002")
-	--gm:sendChatMessage("4f7172b3a821ab0001000002", "blah")
-	--gm:getChatHistory("4f7172b3a821ab0001000002")
-	
-	--gm:createMatch()
-	--gm:getMatches()
-	--gm:acceptChallenge("4f7309d702b79f000100000b")
-	--gm:getMatchDetails("4f72c89e7b57140001000002")
-	--gm:addPlayerToMatch("4f70674d97e9160001000009", "4f7309d702b79f000100000b")
-	--gm:startMatch("4f7309d702b79f000100000b")
-	--gm:submitMove("move 3", nil, nil, "4f71ab40b07bbe0001000003")
-	--gm:getRecentMoves("4f72c89e7b57140001000002")
-	--gm:getAllMoves("4f72c89e7b57140001000002")
-	--gm:createRandomChallenge("4f719f4109451e0001000011")
-	--gm:createMPChannel("4f719f4109451e0001000011")
-	--gm:pollMP("4f7309d702b79f000100000c")
-	--gm:pollMP("4f730a0002b79f000100000d")
-	
-end
-timer.performWithDelay(5000, gmCallListener)
-
-local function mpCallListener()
-	for i=1, 100 do
-		--local content = "move "..i
-		--gm:submitMove(content, nil, nil, "4f7309d702b79f000100000b")
-		--analyticsEvent.value = i
-		--gm:submitEvent(analyticsEvent)
-	end
-	--gm:acceptChallenge("4f71b41173860c0001000001")
-
-end
---timer.performWithDelay(10000, mpCallListener)
+--Here are event listeners that will call the method "listenerLogin" when the response is received
+--Please review the gameminion.lua to see the event names that are returned
+Runtime:addEventListener( "LoggedIn", listenerLogin )
+Runtime:addEventListener( "LoginError", listenerLogin )
